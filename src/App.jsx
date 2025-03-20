90a82234599140d544e81d077c470457f1473cb9
Vaishnavi Shelke <shelkesejal06@gmail.com>
2025-03-21 00:17:36 +0530

    Update App file

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,3 @@
-import { useState } from 'react'
-import viteLogo from '/vite.svg'
 import React, { useEffect } from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
@@ -9,9 +7,9 @@ import FarmerLogin from './pages/FarmerLogin';
 import SocietyLogin from './pages/SocietyLogin';
 import Register from './pages/Register';
 import './styles/Home.css';
-import FarmerSetup from "./components/FarmerSetup";
-import FarmerDashboard from "./components/FarmerDashboard";
-
+import FarmerSetup from "./pages/FarmerSetup";
+import FarmerDashboard from "./pages/FarmerDashboard";
+import { AuthProvider } from "./context/AuthContext"; // Import AuthContext
 
 function App() {
   useEffect(() => {
@@ -30,18 +28,20 @@ function App() {
   }, []);
 
   return (
-    <Router>
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/farmer-login" element={<FarmerLogin />} />
-        <Route path="/society-login" element={<SocietyLogin />} />
-        <Route path="/register" element={<Register />} />
-        <Route path="/farmer-setup" element={<FarmerSetup />} />
-        <Route path="/farmer-dashboard" element={<FarmerDashboard />} />
-      </Routes>
-      <Footer />
-    </Router>
+    <AuthProvider> {/* Wrap everything inside AuthProvider */}
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/farmer-login" element={<FarmerLogin />} />
+          <Route path="/society-login" element={<SocietyLogin />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/farmer-setup" element={<FarmerSetup />} />
+          <Route path="/farmer-dashboard" element={<FarmerDashboard />} />
+        </Routes>
+        <Footer />
+      </Router>
+    </AuthProvider>
   );
 }
 
